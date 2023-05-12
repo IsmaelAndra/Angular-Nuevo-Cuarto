3552b9a98c73c5cd576fb38c6c01d8f7aa0d5bef
MisakiSan3 <iot.gomez@yavirac.edu.ec>
2023-05-12 16:46:26 -0500

    Cambios 12/05/23

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -7,7 +7,7 @@ import { ProductHttpService } from 'src/app/services/product.service'
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductsComponent implements OnInit {
    products:ProductModel[] = [];
    selectedProduct: UpdateProductDto = {title:'', price:0, description:''};
 
@@ -16,11 +16,11 @@ export class ProductComponent implements OnInit {
   }
   
   ngOnInit(): void {
-    //this.getProducts();
+    this.getProducts();
     //this.getProduct(57);
     //this.createProduct();
     //this.updateProduct();
-    this.deleteProduct(204);
+    //this.deleteProduct(204);
   }
 
   getProducts(){
@@ -32,6 +32,7 @@ export class ProductComponent implements OnInit {
       }
     )
   }
+
   getProduct(id: ProductModel['id'] ){
     const url = "https://api.escuelajs.co/api/v1/products/id";
     return this.productHttpService.getOne(id).subscribe(
@@ -41,30 +42,16 @@ export class ProductComponent implements OnInit {
     )
   }
 
-  createProduct(){
-    const data = {
-      title: 'Balon',
-      price: 50,
-      description: 'Balon de futbol profesional - Jhonny Andrade',
-      images: [
-        'https://cdn.shopify.com/s/files/1/0120/6485/0016/products/MK-4.jpg?v=1608244207'
-      ],
-      categoryId: 1,
-    };
-    this.productHttpService.store(data).subscribe(
+  createProduct(product: CreateProductDto){
+    this.productHttpService.store(product).subscribe(
       response => {
         console.log(response);
       }
     )
   }
 
-  updateProduct(){
-    const data = {
-      title: 'Pupos',
-      price: 250,
-      descripcion: 'Pupos Profesionales Mercurial - Jhonny Andrade'
-    };
-    this.productHttpService.update(204, data).subscribe(
+  updateProduct(id: ProductModel['id'], product: UpdateProductDto){
+    this.productHttpService.update(id, product).subscribe(
       response =>{
         console.log(response);
       }
@@ -77,7 +64,7 @@ export class ProductComponent implements OnInit {
   deleteProduct(id: ProductModel['id']){
     this.productHttpService.destroy(id).subscribe(
       response =>{
-        this.products = this.products.filter(product => product.id != id); 
+        this.products = this.products.filter(product => product.id != id);
         console.log(response);
       }
     )
